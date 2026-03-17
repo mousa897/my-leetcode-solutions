55fc77a2bd6fe1f961719272e84d018fb4b128fa
Mousa <mousaalihamadna@gmail.com>
2026-03-17 20:58:06 +0300

    Add function composition solution

---
+++ Javascript/easy/Function-Composition.js
@@ -0,0 +1,17 @@
+// Question:
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function
+// that is the function composition of the array of functions.
+// The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+// The function composition of an empty list of functions is the identity function f(x) = x.
+
+// Solution:
+var compose = function (functions) {
+  return function (x) {
+    let result = x;
+    if (functions.length === 0) return x;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      result = functions[i](result);
+    }
+    return result;
+  };
+};
